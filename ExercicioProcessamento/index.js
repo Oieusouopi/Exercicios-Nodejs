fcd74a1f4869f103ebd349d47bab8759146159d5
Oieusouopi <rafasc866@gmail.com>
2022-06-17 22:34:05 -0300

    processamento

--- ExercicioProcessamento/index.js
+++ ExercicioProcessamento/index.js
@@ -1,3 +1,20 @@
 const os = require('os');
 
-console.log(os.constants);
+
+setInterval( () => {
+    
+    const { freemem, totalmem } = os;
+    const memoryTotal = parseInt(totalmem() / 1024 / 1024);
+    const memoryFree = parseInt(freemem() / 1024 / 1024);
+    const percentFree = parseInt(memoryFree / memoryTotal * 100);
+
+    const statsCpu = {
+        total: `${ memoryTotal }MB`,
+        free: `${ memoryFree }MB`,
+        usage: `${ percentFree }%`,
+    };
+
+    console.clear();
+    console.table(statsCpu);
+
+}, 1000);
